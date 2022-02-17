fb46ef90f280fa27635bb1808c7440c59eb58797
AldairZemanate <zyiber@unicauca.edu.co>
2022-02-17 11:56:15 -0500

    deploy2

--- electricars_core/settings.py
+++ electricars_core/settings.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-
+import cloudinary
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -70,7 +70,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "graphene_django",
-    'cloudinary',
+    #'cloudinary',
     #'cloudinary_storage',
     'corsheaders',
 
