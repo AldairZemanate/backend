39a5be085b88984f22cf26a2412c565101bb5500
AldairZemanate <zyiber@unicauca.edu.co>
2022-02-23 12:28:52 -0500

    deploy2

--- electricars_core/urls.py
+++ electricars_core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from graphene_django.views import GraphQLView
+from graphene_file_upload.django import FileUploadGraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
